1fb9cda2524bae54f2d49f652970006325f0e93c
VivianDee <vdagbue@gmail.com>
2024-01-04 17:10:07 -0800

    fix: extreme case

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -7,7 +7,7 @@ def pascal_triangle(n):
       Returns a list of lists of integers representing
       the Pascal’s triangle of n
     """
-    if n <= 0:
+    if type(n) is not int or n <= 0:
         return []
 
     triangle = []
